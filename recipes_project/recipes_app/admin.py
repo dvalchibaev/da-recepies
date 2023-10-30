6d584a6db55ccc440978326cbd82f3c972df486f
dvalchibaev <alchibaevdv@gmail.com>
2023-10-30 15:59:38 +0400

    Initial models and Database

--- recipes_project/recipes_app/admin.py
+++ recipes_project/recipes_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Recipe, Category
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'time_to_cook')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
